103a576b3f7cf6b4fcd7c4b20260042287ecf147
yunfanlu913 <yunfanlu913@outlook.com>
2017-02-21 19:54:46 +0800

    添加

---
+++ src/p421MaximumXOROfTwoNumbersInAnArray/Solution.java
@@ -0,0 +1,19 @@
+package p421MaximumXOROfTwoNumbersInAnArray;
+
+class Btree{
+	private class Node{
+		int val;
+		Node r; // 0
+		Node l; // 1
+	}
+	public Btree() {
+		
+	}
+}
+public class Solution {
+	public int findMaximumXOR(int[] nums) {
+		int res = 0;
+		Btree bt = new Btree();
+		return res;
+	}
+}
---
+++ src/p476NumberComplement/Solution.java
@@ -0,0 +1,12 @@
+package p476NumberComplement;
+
+import java.util.List;
+
+public class Solution {
+	public static void main(String[] args) {
+		List<String>[] listString;
+		List[] la = new List[10];	
+		
+		
+	}
+}
